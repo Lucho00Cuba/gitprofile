47d3ad02570c2b9ca1824ae5f5572cd617381916
Lucho00Cuba <lomv0209@gmail.com>
2023-10-14 22:39:15 +0200

    post-fork configuration

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,124 +2,140 @@
 
 const config = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (Required)
-    sortBy: 'stars', // stars | updated
+    username: 'Lucho00Cuba', // Your GitHub org/user name. (Required)
+    sortBy: 'updated', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
-      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      projects: [
+        'geo-spy',
+        'continues-delivery',
+        'gitprofile',
+        'php-mailer',
+      ], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
+    linkedin: 'luis-octavio-mota-verdasco-sys-admin',
+    twitter: 'zzjust_mezz',
+    mastodon: '',
     facebook: '',
     instagram: '',
     youtube: '', // example: 'pewdiepie'
     dribbble: '',
     behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
+    medium: '',
+    dev: '',
     stackoverflow: '', // example: '1/jeff-atwood'
     skype: '',
     telegram: '',
-    website: 'https://arifszn.com',
+    website: 'https://lucho00cuba.github.io',
     phone: '',
-    email: 'arifulalamszn@gmail.com',
+    email: 'lomv0209@gmail.com',
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://lucho00cuba.github.io/gitprofile/resume.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    'PHP',
-    'Laravel',
-    'JavaScript',
-    'React.js',
-    'Node.js',
-    'Nest.js',
-    'MySQL',
+    'Bash',
+    'Python',
+    'Go',
+    'SQL',
+    'PowerShell',
+    'Linux',
+    'Proxmox',
     'PostgreSQL',
     'Git',
     'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
+    'Kubernetes',
+    'Docker',
+    'AWS',
+    'Openstack',
+    'Ansible',
+    'Traefik',
+    'HAProxy',
+    'NGINX',
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
+      company: 'Vermont Solutions',
+      position: 'DevOps Engineer',
+      from: 'May 2023',
       to: 'Present',
-      companyLink: 'https://example.com',
+      companyLink: 'https://vermont-solutions.com',
     },
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+      company: 'Vermont Solutions',
+      position: 'System Administrator',
+      from: 'May 2021',
+      to: 'April 2023',
+      companyLink: 'https://vermont-solutions.com',
     },
   ],
-  /* certifications: [
-    {
-      name: 'Lorem ipsum',
-      body: 'Lorem ipsum dolor sit amet',
-      year: 'March 2022',
-      link: 'https://example.com'
-    },
-  ], */
+  // certifications: [
+  //   {
+  //     name: 'Lorem ipsum',
+  //     body: 'Lorem ipsum dolor sit amet',
+  //     year: 'March 2022',
+  //     link: 'https://example.com'
+  //   },
+  // ],
   education: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
+      institution: 'iFP. Innovación en Formación Profesional',
+      degree: 'Administración de sistemas informáticos en red',
+      from: '2021',
+      to: '2023',
     },
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
+      institution: 'iFP. Innovación en Formación Profesional',
+      degree: 'Sistemas MicroInformaticos y Redes',
+      from: '2019',
+      to: '2021',
     },
   ],
 
   // To hide the `My Projects` section, keep it empty.
   externalProjects: [
     {
-      title: 'Project Name',
+      title: 'Proxmox V6',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
+        'Proxmox Virtual Environment, o Proxmox VE, entorno de virtualización de servidores de código abierto',
+      imageUrl: 'https://www.tuxis.nl/wp-content/uploads/2020/11/proxmox-ve-250.png',
+      link: 'https://lucho00cuba.github.io/Proxmox/',
     },
     {
-      title: 'Project Name',
+      title: 'Boxcraft',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
+        'Módulo BoxCraft para establecer un sistema de procesamiento distribuido',
+      imageUrl: 'https://cdn.vectorstock.com/i/preview-1x/57/67/people-loading-boxes-into-cargo-container-vector-31685767.jpg',
+      link: 'https://github.com/Lucho00Cuba/boxcraft',
+    },
+    {
+      title: 'CS-Academy',
+      description:
+        'Laboratorios de Ciberseguridad/Pentesting',
+      imageUrl: 'https://cs-academy.org/img/csacademy-logo-head.png',
+      link: 'https://cs-academy.org',
     },
   ],
   // Display blog posts from your medium or dev account. (Optional)
-  blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
-    limit: 2, // How many posts to display. Max is 10.
-  },
-  googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
-  },
+  // blog: {
+  //   source: 'dev', // medium | dev
+  //   username: 'arifszn', // to hide blog section, keep it empty
+  //   limit: 2, // How many posts to display. Max is 10.
+  // },
+  // googleAnalytics: {
+  //   id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+  // },
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: {
     id: '',
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'wireframe',
+    defaultTheme: 'dark',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
